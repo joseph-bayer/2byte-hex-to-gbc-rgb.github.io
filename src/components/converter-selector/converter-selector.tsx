3986997d145eef808f8200b75a04fdbf747002d0
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2025-10-17 16:11:32 -0500

    Improve accessibility of dropdown trigger

--- src/components/converter-selector/converter-selector.tsx
+++ src/components/converter-selector/converter-selector.tsx
@@ -4,10 +4,12 @@ import {
   Select,
   SelectContent,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 import { ConverterTypes } from "@/constants/converter-types";
+import { SelectGroup } from "@radix-ui/react-select";
 
 interface ConverterSelectorProps {
   value?: string;
@@ -19,18 +21,28 @@ export default function ConverterSelector({
   onChange,
 }: ConverterSelectorProps) {
   return (
-    <Select value={value} onValueChange={onChange}>
-      <SelectTrigger className="w-[300px]">
-        <SelectValue placeholder="Select Converter" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value={ConverterTypes.twoByteHex.toString()}>
-          Two Byte Hex to Pokecrystal RGB
-        </SelectItem>
-        <SelectItem value={ConverterTypes.modernHex.toString()}>
-          Modern Hex to Pokecrystal RGB
-        </SelectItem>
-      </SelectContent>
-    </Select>
+    <>
+      {/* SelectTrigger is a button and buttons require labels for screen readers. Otherwise, the screen reader will announce it just as "button" */}
+      <label htmlFor="converter-select" className="sr-only">
+        Select Converter
+      </label>
+
+      <Select value={value} onValueChange={onChange}>
+        <SelectTrigger id="converter-select" className="w-[300px]">
+          <SelectValue placeholder="Select Converter" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Converters</SelectLabel>
+            <SelectItem value={ConverterTypes.twoByteHex.toString()}>
+              Two Byte Hex to Pokecrystal RGB
+            </SelectItem>
+            <SelectItem value={ConverterTypes.modernHex.toString()}>
+              Modern Hex to Pokecrystal RGB
+            </SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   );
 }
